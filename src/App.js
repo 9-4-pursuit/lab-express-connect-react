d15314d35f92792b96a9dc356a93f13dd9556aae
BertCasale <bthebest55@gmail.com>
2023-04-16 12:15:02 -0400

    list all log titles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import { useState, useEffect } 
-from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import Logs from "./components/Logs";
 import LogNewForm from "./components/LogNewForm";
 import NotFound from "./components/NotFound";
+import Log from "./components/Log";
 
 function App() {
   return (<BrowserRouter>
@@ -15,6 +14,7 @@ function App() {
         <Route path="/" element={<Home/>}/>
         <Route path="/logs" element={<Logs/>}/>
         <Route path="/logs/new" element={<LogNewForm/>}/>
+        <Route path="/logs/:index" element={<Log />}/>
         <Route path="*" element={<NotFound/>}/>
       </Routes>
     </div>
--- src/components/Logs.js
+++ src/components/Logs.js
@@ -1,7 +1,27 @@
+import { useState, useEffect } from "react";
 import axios from "axios";
 const API = process.env.REACT_APP_API_URL
-console.log(API)
+console.log(API);
+
 
 export default function Logs() {
-  return(<div className="logs"> Logs</div>);
+  const [logs, setLogs] = useState([]);
+
+  useEffect(() => {
+    axios
+    .get(`${API}/logs`)
+    .then((res) => setLogs(res.data))
+    .catch((e) => console.error("catch", e))
+  }, []);
+
+  return(<div className="logs"> 
+    <ul>
+      {logs.map((log, index) => {
+        return (<li key={index}>
+          <a href={`/logs/${index}`}> {log.title}</a>
+        </li>)
+      })}
+    </ul>
+    
+  </div>);
 }
