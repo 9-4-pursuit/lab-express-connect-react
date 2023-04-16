333c2a11c174385203ffd1d4c40cb4152f814c98
BertCasale <bthebest55@gmail.com>
2023-04-16 11:17:51 -0400

    setup and installation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import { useState, useEffect } 
+from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
-  return <div>Hello World</div>;
+  return (<BrowserRouter>
+    <div>Hello World</div>
+  </BrowserRouter>);
+
 }
 
 export default App;
