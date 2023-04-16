80377de445bbee9d79522a3a9bc3d835274cf1a8
pjungjs <pjungjs@gmail.com>
2023-04-16 11:34:58 -0400

    initial setup.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import { Routes, Route } from "react-router-dom";
+
+import NavBar from "./Components/Common/NavBar.jsx";
+import Home from "./Components/Pages/Home.jsx";
+
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div className="App">
+      <header>
+        <NavBar />
+      </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </main>
+    </div>
+  );
 }
 
 export default App;
